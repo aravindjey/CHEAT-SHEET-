c8d4a07740a10c9598d57a2cc8cdbe96f6df09d1
aravindjey <89334496+aravindjey@users.noreply.github.com>
2023-12-07 15:38:42 +0530

    Add files via upload

---
+++ typescript.js
@@ -0,0 +1,10 @@
+// in vscode terminal type ➝ npm install -g typescript
+// in same terminal type ➝ tsc -v
+// now create a file <filename.ts>
+var message = "HELLO WORLD!";
+console.log(message);
+// after entering the code again in terminal ➝ tsc <file name.ts>
+// now select filename.js which is created on the left box
+// after selecting filename.js in the terminal box below type node filename.js
+//now out put (HELLO WORLD!) is displayed below on the terminal
+
---
+++ typescript.ts
@@ -0,0 +1,7 @@
+// in vscode terminal type ➝ npm install -g typescript
+// in same terminal type ➝ tsc -v
+// now create a file <filename.ts>
+let message: string = "HELLO WORLD!"
+console.log(message);
+
+// after entering the code again in terminal ➝ tsc <file name.ts>
